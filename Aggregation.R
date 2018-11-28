29d51e7580fc16d8bd909b76c145f257b850b80d
Ana <ana.bozic@fer.hr>
2018-11-28 10:05:17 +0100

    Update Aggregation.R
    "generic version"

--- Aggregation.R
+++ Aggregation.R
@@ -1,5 +1,6 @@
 library(chron)
 library(expss)
+library(lubridate)
 
 file <- "Postaja_Institut_20171017-20181017.csv"
 
@@ -18,77 +19,105 @@ readFile <- function(file) {
   )
 }
 
-#Average value of parameter from start date till end date
-average <- function(start_date, end_date, parameter) {
+#Subsetting part of data set that is needed for calculations
+subset_data <- function(start_date, end_date, data, parameter) {
   calc_data <- list()
   calc_data <-
     subset(data[parameter],
            subset = (data$Timestamp >= start_date &
                        data$Timestamp <= end_date))
-  return(mean(simplify2array(calc_data)))
+  return(simplify2array(calc_data))
 }
 
-#Min, max and range function
-range <- function(start_date, end_date, parameter) {
-  calc_data <- list()
-  calc_data <-
-    subset(data[parameter],
-           subset = (data$Timestamp >= start_date &
-                       data$Timestamp <= end_date))
-  calc_data <- simplify2array(calc_data)
-  max <- max(calc_data)
-  min <- min(calc_data)
-  return(c(max, min, max - min))
+#Average value of parameter from start date till end date
+average_data <- function(dataset) {
+  return(mean(dataset, na.rm = TRUE))
+}
+
+#Range function -> max - min value
+range_data <- function(dataset) {
+  max <- max(dataset, na.rm = TRUE)
+  min <- min(dataset, na.rm = TRUE)
+  return(max - min)
 }
 
 #Sum of parameter from start_date till end_date
-sum_data <- function(start_date, end_date, parameter) {
-  calc_data <- list()
-  calc_data <-
-    subset(data[parameter],
-           subset = (data$Timestamp >= start_date &
-                       data$Timestamp <= end_date))
-  calc_data <- simplify2array(calc_data)
-  return(sum(calc_data))
+sum_data <- function(dataset) {
+  return(sum(dataset, na.rm = TRUE))
 }
 
 #Count all the rows that satisfy the statement
-count_data <- function(start_date,
-                       end_date,
-                       parameter,
+count_data <- function(dataset,
                        statement) {
-  calc_data <- list()
-  calc_data <-
-    subset(data[parameter],
-           subset = (data$Timestamp >= start_date &
-                       data$Timestamp <= end_date))
-  calc_data <- simplify2array(calc_data)
-  n <- count_if(statement, calc_data)
+  n <- count_if(statement, dataset)
   return(n)
 }
 
 #Save rows from data frame that satisfy the statement
-save_data_if <- function(start_date,
-                         end_date,
-                         parameter,
-                         statement) {
-  calc_data <- list()
+saveif_data <- function(start_date, end_date, dataset,
+                        statement) {
   n <- 0
   savedData <- data.frame()
+  calc_data <- list()
   calc_data <-
     subset(data,
            subset = (data$Timestamp >= start_date &
                        data$Timestamp <= end_date))
-  param_data <- simplify2array(calc_data[parameter])
+  param_data <- dataset
   for (i in 1:length(param_data)) {
     n <- count_if(statement, param_data[i])
     if (n == 1) {
-      savedData <- rbind(savedData, calc_data[i, ])
+      savedData <- rbind(savedData, calc_data[i,])
     }
   }
   return(savedData)
 }
 
+#Timestamps by hours
+hours_frame <- function(start_date, end_date) {
+  hourly <- list()
+  h <- start_date
+  hourly <- append(hourly, as.POSIXct(h))
+  while (h < end_date) {
+    hour(h) <- hour(h) + 1
+    if (h <= end_date) {
+      hourly <- append(hourly, as.POSIXct(h))
+    }
+  }
+  return(hourly)
+}
+
+#Timestamps by days
+days_frame <- function(start_date, end_date) {
+  daily <- list()
+  d <- start_date
+  daily <- append(daily, as.POSIXct(d))
+  while (d < end_date) {
+    day(d) <- day(d) + 1
+    if (d <= end_date) {
+      daily <- append(daily, as.POSIXct(d))
+    }
+  }
+  return(daily)
+}
+
+#Timestamps by months
+months_frame <- function(start_date, end_date) {
+  start_date <-
+    as.POSIXct(start_date, format = "%Y-%m-%d")
+  end_date <-
+    as.POSIXct(end_date, format = "%Y-%m-%d")
+  monthly <- list()
+  m <- start_date
+  monthly <- append(monthly, as.POSIXct(m))
+  while (m < end_date) {
+    month(m) <- month(m) + 1
+    if (m <= end_date) {
+      monthly <- append(monthly, as.POSIXct(m))
+    }
+  }
+  return(monthly)
+}
 
 # MAIN
 data <- readFile(file)
@@ -108,45 +137,3 @@ data$Timestamp <-
 data$Timestamp <- gsub("[[:space:]]", "", data$Timestamp)
 data$Timestamp <-
   as.POSIXct(data$Timestamp, format = "%m/%d/%Y %H:%M", tz = Sys.timezone())
-
-#Average function call example
-m <-
-  average(
-    as.POSIXct("2017-10-20 19:35", format = "%Y-%m-%d %H:%M"),
-    as.POSIXct("2017-10-20 23:21", format = "%Y-%m-%d %H:%M"),
-    "Tlak.zraka"
-  )
-
-#Range function call example
-r <-
-  range(
-    as.POSIXct("2017-10-20 19:35", format = "%Y-%m-%d %H:%M"),
-    as.POSIXct("2017-10-20 23:21", format = "%Y-%m-%d %H:%M"),
-    "Tlak.zraka"
-  )
-
-#Sum_dates function call example
-s <-
-  sum_data(
-    as.POSIXct("2017-10-20 19:35", format = "%Y-%m-%d %H:%M"),
-    as.POSIXct("2017-10-20 23:21", format = "%Y-%m-%d %H:%M"),
-    "Brzina.vjetra"
-  )
-
-#Count_if function call example
-c <-
-  count_data(
-    as.POSIXct("2017-10-20 19:35", format = "%Y-%m-%d %H:%M"),
-    as.POSIXct("2017-10-20 23:21", format = "%Y-%m-%d %H:%M"),
-    "Temperatura.zraka",
-    as.criterion(gt(17) & le(18))
-  )
-
-#Save_data_if function call example
-d <-
-  save_data_if(
-    as.POSIXct("2017-10-20 19:35", format = "%Y-%m-%d %H:%M"),
-    as.POSIXct("2017-10-20 23:21", format = "%Y-%m-%d %H:%M"),
-    "Temperatura.zraka",
-    as.criterion(gt(18) & le(18.5))
-  )
